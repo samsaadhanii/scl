ad7a2ed921ebbaf857c631c617d3825d8aae5146
Amba Kulkarni <ambapradeep@gmail.com>
2021-03-11 10:12:45 +0530

    problem with ms fixed

--- SHMT/prog/Normalisation/get_std_spelling_fn.pl
+++ SHMT/prog/Normalisation/get_std_spelling_fn.pl
@@ -27,6 +27,7 @@ $in =~ s/M([tTdDN])/N$1/g;
 $in =~ s/M([wWxXn])/n$1/g;
 $in =~ s/M([pPbBm])/m$1/g;
 $in =~ s/M$/m/g;
+$in =~ s/ms$/Ms/;
 $in =~ s/zS/MS/g;
 #$in =~ s/x$/w/g; THIS IS A WRONG RULE.
 $in =~ s/rww([aeiouAEIOUqQ])/rw$1/g;
