fea55b9896771565bfa2407fba4697ce11de565d
Amba Kulkarni <ambapradeep@gmail.com>
2022-04-04 18:03:08 -0500

    Result Tools not be added now

--- Result_Tools/sp-results/marks.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$DB_SEARCH_SCRIPT = "/home/sanal/test/sp-results/get_marks_info.py";
-// $DB_SEARCH_SCRIPT = "/home/poochoo/Projects/samsaadhanii_praveshika/show_marks/get_marks_info.py";
-$reg_id = $_GET["regid"];
-$info = exec("python3 $DB_SEARCH_SCRIPT $reg_id");
-echo($info);
-
-?>
