9c3ff5088827c1564f2c817c0e84be74c2e0bcb5
Amba Kulkarni <ambapradeep@gmail.com>
2022-12-30 08:47:13 +0530

    added links for each component of a compound

--- MT/prog/interface/add_dict_ref.pl
+++ MT/prog/interface/add_dict_ref.pl
@@ -6,16 +6,23 @@ while($in = <STDIN>){
 if($in =~ /tr class="row[35]"/) { $add_ref=1;}
 if(($in =~ /(.*<td class=[^>]+>[ ]*)(.*)(<\/td>.*)/) && ($add_ref)) {
  $left = $1;
- $middle = $2;
+ $m = $2;
  $right = $3;
 
- $middle =~ s/\(([^ ]*[_\-])?([^0-9\- ]+)([0-9]*)[ ]/\($1<a href="javascript:show('$2','$outscript')">$2$3<AAA>/g;
- $middle =~ s/^([^ ]*[_\-])?([^0-9\- ]+)([0-9]*)[ ]/$1<a href="javascript:show('$2','$outscript')">$2$3<AAA>/;
- $middle =~ s/\/([^ ]*[_\-])?([^0-9\- ]+)([0-9]*)[ ]/\/$1<a href="javascript:show('$2','$outscript')">$2$3<AAA>/g;
- # $middle =~ s/\-([^ ]*_)?([^0-9\- ]+)([0-9]*)[ ]/-$1<a href="javascript:show('$2','$outscript')">$2$3<AAA>/g;
+ print " m = $m\n";
+ $mlink = "";
+ @middle = split('-', $m);
+ foreach $middle (@middle) {
+   $middle =~ s/\(([^ ]*[_\-])?([^0-9\- ]+)([0-9]*)[ ]/\($1<a href="javascript:show('$2','$outscript')">$2$3<AAA>/g;
+   $middle =~ s/^([^ ]*[_\-])?([^0-9\- ]+)([0-9]*)[ ]/$1<a href="javascript:show('$2','$outscript')">$2$3<AAA>/;
+   $middle =~ s/\/([^ ]*[_\-])?([^0-9\- ]+)([0-9]*)[ ]/\/$1<a href="javascript:show('$2','$outscript')">$2$3<AAA>/g;
+   # $middle =~ s/\-([^ ]*_)?([^0-9\- ]+)([0-9]*)[ ]/-$1<a href="javascript:show('$2','$outscript')">$2$3<AAA>/g;
+   $mlink .= "-".$middle;
+}
 
- $middle =~ s/<AAA>/<\/a> /g;
- print $left,$middle,$right,"\n";
+ $mlink =~ s/<AAA>/<\/a> /g;
+ $mlink =~ s/^-//g;
+ print $left,$mlink,$right,"\n";
  $add_ref = 0;
  } elsif ($in =~ /gen.cgi\?filename=\/MT/) {
    $in =~ s/gen.cgi\?filename=\/MT/gen.cgi\?filename=$ARGV[1]\/MT/;
