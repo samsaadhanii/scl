9eba70f0677a49ec698261194f6b36e9a9c11d21
Amba Kulkarni <ambapradeep@gmail.com>
2023-02-10 14:15:05 +0530

    json related bugs fixed

--- MT/prog/reader_generator/csv2json.pl
+++ MT/prog/reader_generator/csv2json.pl
@@ -11,19 +11,17 @@ while($in = <STDIN>){
  chomp($in);
  if($in =~ /./) {
   @flds = split(/\t/,$in);
-  #if($first == 0) { print "},\n";}
   if($first == 0) { print "},";}
-  #print "{\n";
   print "{";
   for ($i=0;$i<=$tot_columns;$i++) { # 
-   if($i==9) { $flds[$i] = "";}  ## This is the English Meaning field, which is currently blank. For MT system for Amruta, we are producing karwari prayoga translation in this field.
-   #print "\"$names[$i]\":\"$flds[$i]\",\n";
-   print "\"$names[$i]\":\"$flds[$i]\",";
+      if($i==9) { $flds[$i] = "";}  ## This is the English Meaning field, which is currently blank. For MT system for Amruta, we are producing karwari prayoga translation in this field.
+      if($i == $tot_columns){
+         print '"'.$names[$i].'":"'.$flds[$i].'"';
+      }else{
+         print '"'.$names[$i].'":"'.$flds[$i].'",';
+      }
   }
-  # print "\"$names[$#flds]\":\"$flds[$#flds]\"\n";
-   print "\"$names[$#flds]\":\"$flds[$#flds]\",";
    $first = 0;
  }
 }
-#print "}\n]\n";
 print "}]";
