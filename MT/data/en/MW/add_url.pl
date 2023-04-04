03e1b04652bdc89e14702ceb9fd0ee08ebd3d0f0
Amba Kulkarni <ambapradeep@gmail.com>
2023-04-04 16:18:22 +0530

    url rectified

--- MT/data/en/MW/add_url.pl
+++ MT/data/en/MW/add_url.pl
@@ -2,7 +2,8 @@
 #
 
 while($in = <STDIN>){
-	$in =~ s/!CGIDECL/\/cgi-bin\/SKT\/sktdeclin/g;
-	$in =~ s/!CGICONJ/\/cgi-bin\/SKT\/sktconjug/g;
+#	$in =~ s/!CGIDECL/\/cgi-bin\/SKT\/sktdeclin/g;
+#	$in =~ s/!CGICONJ/\/cgi-bin\/SKT\/sktconjug/g;
+	$in =~ s/href="([0-9])/href="\/scl\/MT\/data\/en\/MW\/$1/g;
 	print $in;
 }
