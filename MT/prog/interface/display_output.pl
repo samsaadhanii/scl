6a3e60b0cc2e5754b989550f0dbc2053eb32defd
Amba Kulkarni <ambapradeep@gmail.com>
2023-04-17 11:35:24 +0530

    date changed

--- MT/prog/interface/display_output.pl
+++ MT/prog/interface/display_output.pl
@@ -83,7 +83,7 @@ $conv;
               </h3>
             </td>
             <td text-align=\"center\" width=\"25%\">
-              <font color=\"magenta\" size=\"3\">Updated on 27th Jan 2023</font>
+              <font color=\"magenta\" size=\"3\">Updated on 17th Apr 2023</font>
             </td>
             <td text-align=\"right\"> </td>
           </tr>
