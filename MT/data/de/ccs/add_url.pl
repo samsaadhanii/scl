a31ef6102dfdad1087a455a174f8bd8b5e708873
Amba Kulkarni <ambapradeep@gmail.com>
2023-05-05 16:47:10 +0530

    corrected the URL path

--- MT/data/de/ccs/add_url.pl
+++ MT/data/de/ccs/add_url.pl
@@ -7,6 +7,8 @@ while($in = <STDIN>){
 #	$in =~ s/href="([0-9])/href="\/scl\/MT\/data\/de\/css\/$1/g;
 #	$in =~ s/href="\/cgi-bin\/scl/href="\/scl/g;
 #	$in =~ s/scl\/MT\/data\/de\/ccs/SCL_HTDOCS\/MT\/data\/de\/ccs/g;
-	$in =~ s/cgi-bin\/SKT/cgi-bin\/HERITAGE_CGI_DIR/g;
+#	$in =~ s/cgi-bin\/SKT/cgi-bin\/HERITAGE_CGI_DIR/g;
+	$in =~ s/\/cgi-bin\/HERITAGE_CGI_DIR\/sktdeclin/\/cgi-bin\/HERITAGE_CGI_DIR\/sktdeclin.cgi/g;
+	$in =~ s/\/cgi-bin\/HERITAGE_CGI_DIR\/sktconjug/\/cgi-bin\/HERITAGE_CGI_DIR\/sktconjug.cgi/g;
 	print $in;
 }
