28bd558b1ef0df58f51be289fcfd2a445b24bec0
Amba Kulkarni <ambapradeep@gmail.com>
2023-12-26 13:49:15 +0530

    add url for the dict links

--- MT/data/fr/Heritage/add_url.pl
+++ MT/data/fr/Heritage/add_url.pl
@@ -4,8 +4,10 @@
 while($in = <STDIN>){
 #	$in =~ s/!CGIDECL/\/cgi-bin\/SKT\/sktdeclin/g;
 #	$in =~ s/!CGICONJ/\/cgi-bin\/SKT\/sktconjug/g;
-	$in =~ s/href="([0-9])/href="\/scl\/MT\/data\/fr\/Heritage\/$1/g;
-	$in =~ s/href="\/scl\/MT\/data\/fr\/Heritage/href="\/SCL_HTDOCS\/MT\/data\/fr\/Heritage/g;
-	$in =~ s/href="\/cgi-bin\/SKT/href="\/cgi-bin\/HERITAGE_CGI_DIR/g;
+#	$in =~ s/href="([0-9])/href="\/scl\/MT\/data\/fr\/Heritage\/$1/g;
+#	$in =~ s/href="\/scl\/MT\/data\/fr\/Heritage/href="\/SCL_HTDOCS\/MT\/data\/fr\/Heritage/g;
+#	$in =~ s/href="\/cgi-bin\/SKT/href="\/cgi-bin\/HERITAGE_CGI_DIR/g;
+#	$in =~ s/\/sktdeclin\?/\/sktdeclin.cgi\?/g;
+	$in =~ s/\/sktconjug\?/\/sktconjug.cgi\?/g;
 	print $in;
 }
